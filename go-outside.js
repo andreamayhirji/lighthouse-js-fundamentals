127d2f995d98440ce443942cd48dbb96ee499346
Andrea <andreamayhirji@gmail.com>
2018-08-09 22:36:58 +0000

    Adding excercise file go-outside.js

---
+++ go-outside.js
@@ -0,0 +1,35 @@
+/*var cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short seleves are fine.");
+}
+
+var temperature = -4;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf.");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you can go outside.");
+
+
+var temperature = -50;
+
+if (temperature < -40 || temperature > 40){
+  console.log("Maybe you shouldn't go outside today.")
+}
+*/
+
+var raining = true;
+
+if (!raining) {
+  console.log("No need for that umbrella");
+} else  {
+  console.log("Bring your umbrella");
+}
