f28dc3fbe1da35a2414e291e78ae19fdf265648f
Andrea <andreamayhirji@gmail.com>
2018-08-16 16:48:25 +0000

    Add looping assignments

--- chorus-for.js
+++ chorus-for.js
@@ -1,5 +1,5 @@
 var chorus = "Let's dance!";
-for (var repeat = 0; repeat < 10; repeat = repeat + 1) {
+for (var repeat = 0; repeat < 10; repeat++) {
   console.log(chorus);
 }
 console.log("Until the sun comes up!");
